d1b7f759cc4f16938378841fa23330c441a1db8a
AhmedY29 <ahmedys2001@gmail.com>
2025-05-12 14:00:03 +0300

    fix error

--- main.js
+++ main.js
@@ -1,78 +1,108 @@
 let username = document.getElementById("username");
 let postContent = document.getElementById("post-content");
 let postImg = document.getElementById("post-image");
+let errorMsg = document.getElementById("errorMsg");
 let submitBtn = document.getElementById("submit-btn");
 
-function getPostItems() {
-  let posts = document.getElementById("posts");
-  fetch("http://6821ad3d259dad2655b02055.mockapi.io/posts/posts")
-    .then((response) => response.json())
-    .then((data) =>
-      data.map((post) => {
-        let postItem = document.createElement("div");
-        postItem.classList.add("post-item");
+let posts = document.getElementById("posts");
 
-        let postAuthor = document.createElement("div");
-        postAuthor.classList.add("post-author");
+let users = [];
+fetch("https://6821ad3d259dad2655b02055.mockapi.io/posts/posts")
+  .then((response) => response.json())
+  .then((data) =>
+    data.forEach((post) => {
+      let postItem = document.createElement("div");
+      postItem.classList.add("post-item");
 
-        let postAuthorName = document.createElement("h4");
-        postAuthorName.classList.add("post-name-author");
-        postAuthorName.innerText = post.username;
-        let postAuthorAvatar = document.createElement("img");
-        postAuthorAvatar.classList.add("post-name-avatar");
-        postAuthorAvatar.src =
-          "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541";
-        postAuthorAvatar.width = "30";
-        postAuthor.appendChild(postAuthorAvatar);
-        postAuthor.appendChild(postAuthorName);
+      let postAuthor = document.createElement("div");
+      postAuthor.classList.add("post-author");
 
-        let postInfo = document.createElement("div");
-        postInfo.classList.add("post-info");
+      let postAuthorName = document.createElement("h4");
+      postAuthorName.classList.add("post-name-author");
+      postAuthorName.innerText = post.username;
+      let postAuthorAvatar = document.createElement("img");
+      postAuthorAvatar.classList.add("post-name-avatar");
+      postAuthorAvatar.src =
+        "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541";
+      postAuthorAvatar.width = "30";
+      postAuthor.appendChild(postAuthorAvatar);
+      postAuthor.appendChild(postAuthorName);
 
-        let postImage = document.createElement("img");
-        postImage.src = post.img;
-        postImage.classList.add("post-img");
-        postImage.width = "250";
-        postImage.height = "300";
+      let postInfo = document.createElement("div");
+      postInfo.classList.add("post-info");
 
-        let postText = document.createElement("p");
-        postText.classList.add("post-name-author");
-        postText.innerText = post.textarea;
+      let postImage = document.createElement("img");
+      postImage.src = post.img;
+      postImage.classList.add("post-img");
+      postImage.width = "250";
+      postImage.height = "300";
 
-        let postBtns = document.createElement("div");
-        postBtns.classList.add("btns");
+      let postText = document.createElement("p");
+      postText.classList.add("post-name-author");
+      postText.innerText = post.textarea;
 
-        let detailsBtn = document.createElement("button");
-        detailsBtn.classList.add("btn");
-        detailsBtn.classList.add("btn-primary");
-        detailsBtn.innerText = "عرض التفاصيل";
-        let deleteBtn = document.createElement("button");
-        deleteBtn.classList.add("btn");
-        deleteBtn.classList.add("btn-delete");
-        deleteBtn.id = "delete-btn";
-        deleteBtn.innerText = "حذف ";
+      let postBtns = document.createElement("div");
+      postBtns.classList.add("btns");
 
-        postBtns.appendChild(detailsBtn);
-        postBtns.appendChild(deleteBtn);
+      let detailsBtn = document.createElement("button");
+      detailsBtn.classList.add("btn");
+      detailsBtn.classList.add("btn-primary");
+      detailsBtn.innerText = "عرض التفاصيل";
+      let deleteBtn = document.createElement("button");
+      deleteBtn.classList.add("btn");
+      deleteBtn.classList.add("btn-delete");
+      deleteBtn.id = "delete-btn";
+      deleteBtn.innerText = "حذف ";
 
-        postInfo.appendChild(postImage);
-        postInfo.appendChild(postText);
-        postInfo.appendChild(postBtns);
+      deleteBtn.addEventListener("click", () => {
+        fetch(
+          `http://6821ad3d259dad2655b02055.mockapi.io/posts/posts/${post.id}`,
+          {
+            method: "DELETE",
 
-        postItem.appendChild(postAuthor);
-        postItem.appendChild(postInfo);
+            headers: {
+              "Content-type": "application/json; charset=UTF-8",
+            },
+          }
+        );
+      });
 
-        posts.appendChild(postItem);
-      })
-    );
-}
+      postBtns.appendChild(detailsBtn);
+      postBtns.appendChild(deleteBtn);
 
-getPostItems();
+      postInfo.appendChild(postImage);
+      postInfo.appendChild(postText);
+      postInfo.appendChild(postBtns);
+
+      postItem.appendChild(postAuthor);
+      postItem.appendChild(postInfo);
+
+      posts.appendChild(postItem);
+
+      users.push(post.username);
+    })
+  );
 
 submitBtn.addEventListener("click", () => {
-  console.log(username.value, postContent.value, postImg.value);
-
-  fetch("http://6821ad3d259dad2655b02055.mockapi.io/posts/posts", {
+  console.log(users.includes(username.value));
+  if (username.value) {
+    if (users.includes(username.value)) {
+      errorMsg.innerText = "اسم المستخدم موجود بالفعل";
+      return;
+    }
+  } else {
+    errorMsg.innerText = "أدخل اسم المستخدم";
+    return;
+  }
+  if (postImg.value == false) {
+    errorMsg.innerText = "أدخل رابط الصورة";
+    return;
+  }
+  if (postContent.value == false) {
+    errorMsg.innerText = "أدخل محتوى البوست ";
+    return;
+  }
+  fetch("https://6821ad3d259dad2655b02055.mockapi.io/posts/posts", {
     method: "POST",
     body: JSON.stringify({
       username: username.value,
@@ -84,30 +114,8 @@ submitBtn.addEventListener("click", () => {
     },
   });
 
-  setTimeout(() => {
-    getPostItems();
-  }, 500);
-});
-
-let deleteBtn = document.getElementById("delete-btn");
-
-console.log(deleteBtn);
-deleteBtn.addEventListener("click", () => {
-  console.log(username.value, postContent.value, postImg.value);
-
-  //   fetch("http://6821ad3d259dad2655b02055.mockapi.io/posts/posts", {
-  //     method: "POST",
-  //     body: JSON.stringify({
-  //       username: username.value,
-  //       textarea: postContent.value,
-  //       img: postImg.value,
-  //     }),
-  //     headers: {
-  //       "Content-type": "application/json; charset=UTF-8",
-  //     },
-  //   });
-
-  //   setTimeout(() => {
-  //     getPostItems();
-  //   }, 500);
+  errorMsg.innerText = "";
+  username.value = "";
+  postContent.value = "";
+  postImg.value = "";
 });
